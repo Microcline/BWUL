acf995068a1d3df91215d42cfe0c63e05ce90399
Microcline <Microcline@users.noreply.github.com>
2023-01-09 16:46:48 -0600

    Added a comment

--- src/BWULMain.java
+++ src/BWULMain.java
@@ -1,5 +1,7 @@
+// Author Jacob Sherrill
+
 public class BWULMain {
-    public static void main(String args []) {
+    public static void main(String args[]) {
         BWUL bandwidth = new BWUL();
         bandwidth.promptForData();
         System.out.println(bandwidth.toString());
